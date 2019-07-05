bba326decc1716a48283b30f6c8ef2b42a2370ed
Abhay Agarwal <51451528+Abhay-official@users.noreply.github.com>
2019-07-05 10:56:42 +0530

    Add files via upload

---
+++ Day09/Day9I.py
@@ -0,0 +1,13 @@
+class A:
+    def first_method(self):
+        print('Method of class A...')
+
+class B(A):
+    def first_method(self):
+        print('Method of class B...')
+        super().first_method()
+
+if __name__ == '__main__':
+    ob = B()
+    ob.first_method()
+    
