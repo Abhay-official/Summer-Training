b0cafab3ad5a86b056a6e793d1286190b23c9553
Abhay Agarwal <51451528+Abhay-official@users.noreply.github.com>
2019-07-05 10:52:00 +0530

    Add files via upload

---
+++ Day04/Day4B.py
@@ -0,0 +1,8 @@
+fo = open(r'C:\Python\abc.txt', 'w')
+print("Name of file: ", fo.name)
+print("Closed or not? ", fo.closed)
+print("Opening mode: ", fo.mode)
+fo.close()
+print("Closed or not? ", fo.closed)
+
+
